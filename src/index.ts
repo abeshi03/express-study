dc39d8d80f41aa227ada388dcbbaf14967e1c70f
abeshi03 <k_913411@yahoo.co.jp>
2022-01-28 02:08:10 +0900

    expressのtypescript対応修正

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { Application } from "express";
+import { router } from "./interfaces/router";
 
-const app = express();
-const port = 3000;
+const app: Application = express();
+const port = process.env.PORT || 3000;
 
-app.use("/", require("./interfaces/router.js"));
+app.use("/", router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- src/interfaces/router.ts
+++ src/interfaces/router.ts
@@ -1,12 +1,12 @@
-import express from "express";
-const router: express.Router = express.Router();
+import express, { Request, Response, Router } from "express";
+const router: Router = express.Router();
 
-router.get("/", (req: express.Request, res: express.Response) => {
+router.get("/", (req: Request, res: Response) => {
   res.send("Hello world!")
 });
 
 // ルーティングの確認とJsonデータの返し方の確認
-router.get("/test", (req: express.Request, res: express.Response) => {
+router.get("/test", (req: Request, res: Response) => {
   res.json({
     user: {
       name: "test_name",
