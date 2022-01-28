a473d36e69b294f37a508db601247a263a105b64
abeshi03 <k_913411@yahoo.co.jp>
2022-01-28 10:56:57 +0900

    prisma: ハンズオン！

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import { router } from "./interfaces/router";
+import { PrismaClient } from '@prisma/client'
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -10,4 +11,41 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
+const prisma = new PrismaClient();
 
+async function main() {
+
+  // await prisma.user.create({
+  //   data: {
+  //     name: "Alice",
+  //     email: "alice@prisma.io",
+  //     posts: {
+  //       create: { title: "HELLO WORLD" }
+  //     },
+  //     profile: {
+  //       create: { bio: "I like apple" }
+  //     },
+  //   },
+  // })
+
+  // const allUsers = await prisma.user.findMany({
+  //   include: {
+  //     posts: true,
+  //     profile: true
+  //   }
+  // })
+
+  const post = await prisma.post.update({
+    where: { id: 2 },
+    data: { published: true },
+  })
+  console.log(post)
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
