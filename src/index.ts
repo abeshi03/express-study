f39e8896532d7bbfd5db1682ed59add04997224b
abeshi03 <k_913411@yahoo.co.jp>
2022-01-29 16:41:33 +0900

    ルーター修正

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import express, { Application } from "express";
+import { PrismaClient } from '@prisma/client'
+import { userRoutes } from "./interfaces/routes/user";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
+
+
+const prisma = new PrismaClient();
+
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello world!")
+});
+
+app.use("/users", userRoutes(prisma));
+
+app.get("*", (req, res) => {
+  return res.status(400).send({ error: "Invalid Url" });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
