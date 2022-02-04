c8288df35f31e049d054d74b08212b6e23687410
abeshi03 <k_913411@yahoo.co.jp>
2022-02-04 10:28:56 +0900

    UserRepository: クエリパラメーターの処理を追加

--- src/interfaces/database/PostgreSQL/UserRepositoryImpl.ts
+++ src/interfaces/database/PostgreSQL/UserRepositoryImpl.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 import { IUserRepository } from "../repository/IUserRepository";
 import { User, CreateUserPayload } from "../../../domain/User";
+import { FindUserListParams } from "../../request/user/FindUserListRequest";
 
 
 class UserRepository implements IUserRepository {
@@ -11,10 +12,42 @@ class UserRepository implements IUserRepository {
     this.prisma = prisma;
   }
 
-  public async findList(): Promise<User[]> {
-    const users = await this.prisma.user.findMany();
+  public async findList(query: FindUserListParams): Promise<User[]> {
+    const users = await this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        email: true
+      },
+      where: {
+        name: {
+          contains: query.searchByUserName
+        }
+      },
+      orderBy: {
+        id: "asc"
+      },
+      take: query.itemsCountPerPaginationPage,
+      skip:
+        (query.paginationPageNumber -1) * query.itemsCountPerPaginationPage
+    });
 
     return users.map((user: CreateUserPayload) => new User(user));
+  }
+
+  public async totalItemsCount(): Promise<number> {
+    return await this.prisma.user.count();
+  }
+
+  public async ItemsCountInSelection(query: FindUserListParams): Promise<number> {
+    return await this.prisma.user.count({
+      where: {
+        name: {
+          contains: query.searchByUserName
+        }
+      }
+    });
   }
 }
 
