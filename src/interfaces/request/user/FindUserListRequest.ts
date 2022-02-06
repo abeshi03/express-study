bd13a77b17ed4c712d52509b248fffcd43f66b9a
abeshi03 <k_913411@yahoo.co.jp>
2022-02-06 18:30:31 +0900

    コメントを追加

--- src/interfaces/request/user/FindUserListRequest.ts
+++ src/interfaces/request/user/FindUserListRequest.ts
@@ -1,6 +1,7 @@
 import { Request } from "express";
 
 
+/* itemsCountPerPaginationPage, paginationPageNumberは0より大きい整数 */
 export interface FindUserListParams {
   paginationPageNumber?: number;
   itemsCountPerPaginationPage?: number;
