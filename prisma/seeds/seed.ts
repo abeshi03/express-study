eb804663928e0a8bb9f364afa58aac716043af41
abeshi03 <k_913411@yahoo.co.jp>
2022-02-08 00:23:19 +0900

    avatarUri: migrate

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -10,114 +10,133 @@ const usersData: Prisma.UserCreateInput[] = [
     email: "example1@test.com",
     name: "ユーザー1",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example2@test.com",
     name: "ユーザー2",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example3@test.com",
     name: "ユーザー3",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example4@test.com",
     name: "ユーザー4",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example5@test.com",
     name: "ユーザー5",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example6@test.com",
     name: "ユーザー6",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example7@test.com",
     name: "ユーザー7",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example8@test.com",
     name: "ユーザー8",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example9@test.com",
     name: "ユーザー9",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example10@test.com",
     name: "ユーザー10",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example11@test.com",
     name: "ユーザー11",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example12@test.com",
     name: "ユーザー12",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example13@test.com",
     name: "ユーザー13",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example14@test.com",
     name: "ユーザー14",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example15@test.com",
     name: "ユーザー15",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example16@test.com",
     name: "ユーザー16",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example17@test.com",
     name: "ユーザー17",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example18@test.com",
     name: "ユーザー18",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
     email: "example19@test.com",
     name: "ユーザー19",
     description: "宜しくお願いします",
+    avatarUri: "http://placekitten.com/200/300",
     createdAt: moment().format()
   },
   {
