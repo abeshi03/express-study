790348156afe81e4979bcd6caae7293481a3bd19
abeshi03 <k_913411@yahoo.co.jp>
2022-02-11 12:39:31 +0900

    serverを分離

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from '@prisma/client'
 import { userRoutes } from "./interfaces/routes/userRouter";
 
 const app: Application = express();
-const port = process.env.PORT || 5000;
 
 
 const prisma = new PrismaClient();
@@ -20,8 +19,4 @@ app.get("*", (req, res) => {
   return res.status(400).send({ error: "Invalid Url" });
 });
 
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
-
+export default app;
