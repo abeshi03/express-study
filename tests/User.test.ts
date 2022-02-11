dc6a74f31802765fb3c33f8aa527fae6821587ae
abeshi03 <k_913411@yahoo.co.jp>
2022-02-11 23:22:53 +0900

    追加、更新、削除のテストを追加

--- tests/User.test.ts
+++ tests/User.test.ts
@@ -1,10 +1,17 @@
 /* --- フレームワーク、ライブラリー --------------------------------------------------------------------------------------- */
 import request from "supertest"
 import app from "../src";
-import { UserResponse, UsersResponse } from "../src/interfaces/serializers/UserSerializer";
+import { UserResponse } from "../src/interfaces/serializers/UserSerializer";
 
 
 const baseUrl = "/users"
+
+const testData = {
+  email: "update@update.com",
+  name: "update test",
+  description: "update test!!",
+  avatarUri: "http://placekitten.com/200/300"
+}
 
 export function userTest(user: UserResponse): void {
   expect(typeof user.id).toBe("number");
@@ -39,7 +46,7 @@ describe("GET /users", () => {
 
     const users: UserResponse[] = res.body.data.users;
 
-    expect(users.length).toBe(itemsCountPerPaginationPage)
+    expect(users.length).toBe(itemsCountPerPaginationPage);
 
     users.map((user) => {
       userTest(user);
@@ -84,14 +91,97 @@ describe("GET /users", () => {
 describe("GET:: /users/{id}", () => {
 
   it ("Should get user", async () => {
-    const res = await request(app).get(`${baseUrl}/1`)
+
+    const res = await request(app).get(`${baseUrl}/2`);
+
     expect(res.status).toEqual(200);
     expect(res.body.message).toEqual("Success");
     userTest(res.body.data);
   });
 
   it ("Invalid id", async () => {
-    const res = await request(app).get(`${baseUrl}/'1'`)
+
+    const res = await request(app).get(`${baseUrl}/'2'`);
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("Invalid id");
+  });
+
+});
+
+
+describe("POST:: /users", () => {
+
+  it ("Should create user", async () => {
+    const createUserResponse = await request(app).post(baseUrl).send({
+      ...testData,
+      email: "create@create.com"
+    })
+
+    expect(createUserResponse.status).toEqual(200);
+    expect(createUserResponse.body.message).toEqual("Success");
+
+    await request(app).delete(`${baseUrl}/${createUserResponse.body.data.id}`)
+  });
+
+});
+
+
+describe("PATCH:: /users/{id}", () => {
+
+  it ("Should update user", async () => {
+
+    const updateResponse = await request(app).patch(`${baseUrl}/2`).send(testData);
+
+    expect(updateResponse.status).toEqual(200);
+    expect(updateResponse.body.message).toEqual("Success");
+
+    const getUserResponse = await request(app).get(`${baseUrl}/2`);
+    expect(getUserResponse.body.data.name).toEqual("update test");
+  });
+
+  it ("Invalid id", async () => {
+
+    const res = await request(app).patch(`${baseUrl}/'2'`).send(testData);
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("Invalid id");
+  });
+
+  it ("AvatarUri is null", async () => {
+
+    const updateUserResponse = await request(app).patch(`${baseUrl}/2`).send({
+      ...testData,
+      avatarUri: null
+    });
+
+    expect(updateUserResponse.status).toEqual(200);
+    expect(updateUserResponse.body.message).toEqual("Success");
+
+    const getUserResponse = await request(app).get(`${baseUrl}/2`);
+    expect(getUserResponse.body.data.avatarUri).toBeUndefined();
+  });
+
+});
+
+
+describe("DELETE:: /users/{id}", () => {
+
+  it ("should delete user", async () => {
+
+    const createUserResponse = await request(app).post(baseUrl).send({
+      ...testData,
+      email: "create@create.com"
+    });
+
+    const res = await request(app).delete(`${baseUrl}/${createUserResponse.body.data.id}`);
+    expect(res.status).toEqual(200);
+    expect(res.body.message).toEqual("Success");
+  });
+
+
+  it ("Invalid id", async () => {
+    const res = await request(app).delete(`${baseUrl}/'2'`)
     expect(res.status).toEqual(422);
     expect(res.body.message).toEqual("Invalid id");
   });
