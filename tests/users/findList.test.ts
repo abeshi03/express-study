df3b3124040453e00713d643a8fb62ab65aed82b
abeshi03 <k_913411@yahoo.co.jp>
2022-02-11 14:33:58 +0900

    fix: test

--- tests/users/findList.test.ts
+++ tests/users/findList.test.ts
@@ -1,16 +1,20 @@
 /* --- フレームワーク、ライブラリー --------------------------------------------------------------------------------------- */
 import request from "supertest"
 import app from "../../src";
-import {UsersResponse} from "../../src/interfaces/serializers/UserSerializer";
+import { UsersResponse } from "../../src/interfaces/serializers/UserSerializer";
 
 
 describe("GET /users", () => {
 
-  const url = "/users"
+  const baseUrl = "/users"
+  const paginationPageNumber = 4;
+  const itemsCountPerPaginationPage = 4;
 
   it("成功の場合", async () => {
 
-    const res = await request(app).get(`${url}?paginationPageNumber=1&itemsCountPerPaginationPage=1`);
+    const res = await request(app).get(
+      `${baseUrl}?paginationPageNumber=${paginationPageNumber}&itemsCountPerPaginationPage=${itemsCountPerPaginationPage}`
+    );
 
     expect(res.status).toEqual(200);
     expect(res.body.message).toEqual("Success");
@@ -18,11 +22,14 @@ describe("GET /users", () => {
     expect(typeof res.body.data.itemsCountInSelection).toBe("number")
     // TODO
     expect(typeof res.body.data.users).toBe("object")
+
+    const users: UsersResponse[] = res.body.data.users;
+    expect(users.length).toBe(itemsCountPerPaginationPage)
   });
 
   it("クエリパラメーターなしの場合", async () => {
 
-    const res = await request(app).get(url);
+    const res = await request(app).get(baseUrl);
 
     expect(res.status).toEqual(422);
     expect(res.body.message).toEqual("paginationPageNumber is missing");
@@ -30,18 +37,26 @@ describe("GET /users", () => {
 
   it("paginationPageNumberがnumberでは無い場合", async () => {
 
-    const res = await request(app).get(`${url}?paginationPageNumber=string`);
+    const res = await request(app).get(`${baseUrl}?paginationPageNumber=string`);
 
     expect(res.status).toEqual(422);
     expect(res.body.message).toEqual("Invalid paginationPageNumber");
   })
 
-  it("paginationPageNumberだけの場合", async () => {
+  it("itemsCountPerPaginationPageがない場合", async () => {
 
-    const res = await request(app).get(`${url}?paginationPageNumber=5`);
+    const res = await request(app).get(`${baseUrl}?paginationPageNumber=5`);
 
     expect(res.status).toEqual(422);
     expect(res.body.message).toEqual("itemsCountPerPaginationPage is missing");
+  });
+
+  it("itemsCountPerPaginationPageがnumberでは無い場合", async () => {
+
+    const res = await request(app).get(`${baseUrl}?paginationPageNumber=1&itemsCountPerPaginationPage=string`);
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("invalid itemsCountPerPaginationPage");
   });
 
 });
