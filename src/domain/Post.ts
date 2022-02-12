c751e21b86bdef1ebc644644ba25c97b85ecc789
abeshi03 <k_913411@yahoo.co.jp>
2022-02-12 21:45:23 +0900

    postの実態を修正

--- src/domain/Post.ts
+++ src/domain/Post.ts
@@ -1,17 +1,28 @@
+/* --- 実態 ---------------------------------------------------------------------------------------------------------- */
+import { CreateUserPayload, User } from "./User";
+
 interface CreatePostPayload {
   id: number;
+  userId: number;
   content: string;
   imageUri?: string | null;
+  user: CreateUserPayload;
 }
 
 
 class Post {
   private readonly _id: number;
+  private readonly _userId: number;
   private readonly _content: string;
   private readonly _imageUri?: string | null;
+  private readonly _user: User;
 
   public get id(): number {
     return this._id;
+  }
+
+  public get userId(): number {
+    return this.userId;
   }
 
   public get content(): string {
@@ -23,11 +34,17 @@ class Post {
     return this._imageUri;
   }
 
+  public get user(): User {
+    return this._user;
+  }
+
 
   public constructor(payload: CreatePostPayload) {
     this._id = payload.id;
+    this._userId = payload.userId;
     this._content = payload.content;
     this._imageUri = payload.imageUri;
+    this._user = new User(payload.user);
   }
 }
 
