a7d7e9ee5d58fefec196137b73597bcc84336130
abeshi03 <k_913411@yahoo.co.jp>
2022-03-26 16:22:34 +0900

    会員登録の失敗パターンのテストの追加

--- tests/Auth.test.ts
+++ tests/Auth.test.ts
@@ -2,11 +2,18 @@
 import request from "supertest"
 import app from "../src";
 
-/* --- 型定義 --------------------------------------------------------------------------------------------------------- */
+/* --- 実態 ----------------------------------------------------------------------------------------------------------- */
+import { User } from "../src/domain/User";
+
+/* --- リクエスト ------------------------------------------------------------------------------------------------------ */
+import { SignUpRequest } from "../src/interfaces/request/auth/SignUpRequest";
+
+/* --- レスポンス ------------------------------------------------------------------------------------------------------ */
 import { UserResponse } from "../src/interfaces/serializers/UserSerializer";
 
 /* --- 補助関数 ------------------------------------------------------------------------------------------------------- */
 import { userTest } from "./User.test";
+
 
 const baseUrl = "/auth";
 
@@ -82,6 +89,141 @@ describe("POST /signIn", () => {
 
     expect(res.status).toEqual(422);
     expect(res.body.message).toEqual("password must be 4 - 255 character long");
+  });
+});
+
+describe("POST: /signUp", () => {
+
+  const signUpTestData = {
+    email: "signUpTestData@signUpTestData.com",
+    name: "signUpTest",
+    password: "password",
+    description: "signUpTest",
+    avatarUri: "http://placekitten.com/200/300"
+  }
+
+  it("Should signUp", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData
+    });
+
+    expect(res.status).toEqual(200);
+    expect(res.body.message).toEqual("Success");
+
+    const user: User = res.body.data;
+    userTest(user);
+
+    await request(app).delete(`/users/${user.id}`)
+  });
+
+  it("name is missing", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      email: undefined
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("email is missing");
+  });
+
+  it("Invalid email", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      email: 1111
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("Invalid email");
+  });
+
+  it("password is missing", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      password: undefined
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("password is missing");
+  });
+
+  it("Invalid email", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      password: 1234
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("Invalid password");
+  });
+
+  it("password is 3 character", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      password: "123"
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("password must be 4 - 255 character long");
+  });
+
+  it("password is 256 character", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      password: "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("password must be 4 - 255 character long");
+  });
+
+  it("name is missing", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      name: undefined
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("name is missing");
+  });
+
+  it("Invalid name", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      name: 1234
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("Invalid name");
+  });
+
+  it("description is missing", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      description: undefined
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("description is missing");
+  });
+
+  it("Invalid description", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      description: 4243
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("Invalid description");
+  });
+
+  it("description is 201 character", async () => {
+    const res = await request(app).post(`${baseUrl}/signUp`).send({
+      ...signUpTestData,
+      description: "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    });
+
+    expect(res.status).toEqual(422);
+    expect(res.body.message).toEqual("description must be 0 - 200 character long");
   });
 
 });
