85f141a4252c94ac0f666e1ee2bb664cc6b7fc73
abeshi03 <k_913411@yahoo.co.jp>
2022-04-10 13:17:51 +0900

    コメント取得のテストを追加

---
+++ tests/CommentTest.ts
@@ -0,0 +1,48 @@
+/* --- フレームワーク、ライブラリー --------------------------------------------------------------------------------------- */
+import request from "supertest"
+import app from "../src";
+
+/* --- レスポンス ------------------------------------------------------------------------------------------------------ */
+import { CommentResponse, CommentsResponse } from "../src/interfaces/serializers/CommentSerializer";
+
+
+const baseUrl = "/post-comments";
+
+
+export function commentTest(comment: CommentResponse): void {
+  expect(typeof comment.id).toBe("number");
+  expect(typeof comment.text).toBe("string");
+  expect(typeof comment.text).toBeLessThanOrEqual(280);
+  expect(typeof comment.commentedDateTime).toBe("string");
+  expect(typeof comment.commentedUserData.id).toBe("number");
+  expect(typeof comment.commentedUserData.name).toBe("string");
+
+  if (comment.commentedUserData.avatarUri) {
+    expect(typeof comment.commentedUserData.avatarUri).toBe("string");
+  }
+}
+
+
+describe("GET:: /post-comments", () => {
+
+  const limit = 5;
+  const pageNumber = 1;
+
+  it("Should get comment list", async () => {
+
+    const res = await request(app).get(
+      `${baseUrl}/1/?limit=${limit}&pageNumber=${pageNumber}`
+    );
+
+    expect(res.status).toEqual(200);
+    const responseData: CommentsResponse = res.body.data;
+    expect(typeof responseData.totalItemsCount).toEqual("number");
+
+    expect(responseData.comments.length).toBe(limit);
+
+    responseData.comments.forEach((comment) => {
+      commentTest(comment);
+    });
+  });
+
+});
